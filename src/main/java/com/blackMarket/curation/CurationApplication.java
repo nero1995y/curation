a27df172f49d0d0275af3df5daf97829585bb908
nero <wnsgur765z@naver.com>
2023-05-15 23:36:02 +0900

    Initial commit

---
+++ src/main/java/com/blackMarket/curation/CurationApplication.java
@@ -0,0 +1,13 @@
+package com.blackMarket.curation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blackMarket/curation/CurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackMarket.curation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
