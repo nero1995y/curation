160ba65a2eb1bec90c0395a34eda0e898a6628e5
nero <wnsgur765z@naver.com>
2023-06-17 21:33:38 +0900

    Test: add PostRepositoryTest 단위 테스트

--- src/test/java/com/blackMarket/curation/domain/post/repository/PostRepositoryTest.java
+++ src/test/java/com/blackMarket/curation/domain/post/repository/PostRepositoryTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 @DataJpaTest
 class PostRepositoryTest {
@@ -50,5 +53,61 @@ class PostRepositoryTest {
 
         //then
         assertThat(findPost).isEqualTo(save);
+    }
+
+    @DisplayName("게시글 조회 결과 수가 0 이다.")
+    @Test
+    void findSize() {
+        //given
+
+        //when
+        List<Post> result = postRepository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(0);
+    }
+
+    @DisplayName("게시글 조회 결과가 2이다.")
+    @Test
+    void postSize2() {
+        //given
+        Post post = Post.builder()
+                .title("newTitle")
+                .content("newContent")
+                .build();
+
+        Post post2 = Post.builder()
+                .title("newTitle")
+                .content("newContent")
+                .build();
+
+        postRepository.save(post);
+        postRepository.save(post2);
+        //when
+        List<Post> result = postRepository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+    @DisplayName("게시글이 삭제된다")
+    @Test
+    void remove() {
+        //given
+        Post post = Post.builder()
+                .title("newTitle")
+                .content("newContent")
+                .build();
+
+        Post save = postRepository.save(post);
+
+        //when
+        postRepository.deleteById(save.getId());
+
+        //then
+        Optional<Post> findPost = postRepository.findById(post.getId());
+
+        assertThat(findPost).isEmpty();
+
     }
 }
